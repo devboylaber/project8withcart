fcdc663413157fb5f66475ac5f892e86d32ff8e6
devboylaber <devboy.laber@gmail.com>
2014-11-19 19:19:08 -0500

    have the views almost figured out

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,19 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  #these were all added
+  helper_method :current_order
+
+  # def current_order
+  #   if !session[:order_id].nil?
+  #     Order.find(session[:order_id])
+  #   else
+  #     Order.new
+  #   end
+  # end
+      def current_order
+        @current_order ||= Order.find(session[:order_id]) if session[:order_id]
+      end
+  #till here
+end
+    
---
+++ db/seeds.rb
@@ -0,0 +1,52 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+puts "Generating sample products"
+4.times do |n|
+  Product.create(name: "product Name", 
+  	             description: "Lorem ipsum dolor", 
+  	             # image: File.new("#{Rails.root}/public/images/atlantic-salmon#{n}.jpg",
+  	             	price: 0.29,
+                  active: true)
+                 # quantity: 1
+  puts "product  created"
+end
+puts "Sample products created"
+OrderStatus.delete_all
+OrderStatus.create! id: 1, name: "In Progress"
+OrderStatus.create! id: 2, name: "Placed"
+OrderStatus.create! id: 3, name: "Shipped"
+OrderStatus.create! id: 4, name: "Cancelled"
+puts "Generating sample users"
+Order.create! id: 1
+1.times do |n|
+  User.create(email: "user@user.com", 
+  	             password: "useruser",
+  	             password_confirmation: "useruser")
+puts "user created"
+end
+# puts "Sample users created"
+puts "Generating sample users"
+1.times do |n|
+  User.create(email: "admin@admin.com", 
+  	             password: "adminadmin",
+  	             password_confirmation: "adminadmin",  
+  	             admin: true)
+  puts "admin created"
+end
+# puts "Sample admins created"
+puts "Generating sample super admins"
+1.times do |n|
+  User.create(email: "super@admin.com", 
+  	             password: "superadmin",
+  	             password_confirmation: "superadmin",  
+  	             super_admin: true)
+  puts "super admin  created"
+end
+# puts "Sample super admins created"

f38871c44b18d2514edd2d5a83095df3954d6c0a
devboylaber <devboy.laber@gmail.com>
2014-11-20 14:33:17 -0500

    took away remote true and it works

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 
 
 puts "Generating sample products"
-4.times do |n|
+1.times do |n|
   Product.create(name: "product Name", 
   	             description: "Lorem ipsum dolor", 
   	             image: File.new("#{Rails.root}/public/images/atlantic-salmon#{n}.jpg"),
@@ -20,33 +20,30 @@ end
 puts "Sample products created"
 OrderStatus.delete_all
 OrderStatus.create! id: 1, name: "In Progress"
-OrderStatus.create! id: 2, name: "Placed"
-OrderStatus.create! id: 3, name: "Shipped"
-OrderStatus.create! id: 4, name: "Cancelled"
 puts "Generating sample users"
 Order.create! id: 1
-1.times do |n|
-  User.create(email: "user@user.com", 
-  	             password: "useruser",
-  	             password_confirmation: "useruser")
-puts "user created"
-end
-# puts "Sample users created"
-puts "Generating sample users"
-1.times do |n|
-  User.create(email: "admin@admin.com", 
-  	             password: "adminadmin",
-  	             password_confirmation: "adminadmin",  
-  	             admin: true)
-  puts "admin created"
-end
-# puts "Sample admins created"
-puts "Generating sample super admins"
-1.times do |n|
-  User.create(email: "super@admin.com", 
-  	             password: "superadmin",
-  	             password_confirmation: "superadmin",  
-  	             super_admin: true)
-  puts "super admin  created"
-end
-# puts "Sample super admins created"
+# 1.times do |n|
+#   User.create(email: "user@user.com", 
+#   	             password: "useruser",
+#   	             password_confirmation: "useruser")
+# puts "user created"
+# end
+# # puts "Sample users created"
+# puts "Generating sample users"
+# 1.times do |n|
+#   User.create(email: "admin@admin.com", 
+#   	             password: "adminadmin",
+#   	             password_confirmation: "adminadmin",  
+#   	             admin: true)
+#   puts "admin created"
+# end
+# # puts "Sample admins created"
+# puts "Generating sample super admins"
+# 1.times do |n|
+#   User.create(email: "super@admin.com", 
+#   	             password: "superadmin",
+#   	             password_confirmation: "superadmin",  
+#   	             super_admin: true)
+#   puts "super admin  created"
+# end
+# # puts "Sample super admins created"
